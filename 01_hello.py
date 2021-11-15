3a5c03699d91de061b192406390db4ab5f18da4e
Somesh banerjee <32162479+Somesh11097@users.noreply.github.com>
2021-11-15 19:24:09 +0530

    Commit

--- 01_hello.py
+++ 01_hello.py
@@ -1,2 +1,7 @@
+# comments
+'''Author: Somesh
+Welcome to the basics of python'''
+
+
 import os 
 print("hello World")
---
+++ chapter2/var.py
@@ -0,0 +1,8 @@
+# a = "Somesh"
+# b = 345
+# d = False
+# print(b)
+# print(a)
+# print(d)
+d = 3.4
+print(d)
