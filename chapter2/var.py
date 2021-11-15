7c8fb939a83bb4a5789ebb64da5e969145a57b4f
Somesh banerjee <32162479+Somesh11097@users.noreply.github.com>
2021-11-15 20:07:20 +0530

    Update var.py

--- chapter2/var.py
+++ chapter2/var.py
@@ -1,8 +1,29 @@
-# a = "Somesh"
-# b = 345
-# d = False
-# print(b)
-# print(a)
-# print(d)
+a = "Somesh"
+b = 345
+
+print(b)
+print(a)
+
 d = 3.4
 print(d)
+print(type(d))
+
+str1 = "This is my 1st python String"
+print(str1.find("This"))
+
+
+# item = [1,2,3]
+# item[0] = "Somesh"
+# print(item)
+
+# tup1 = (1,2,3)
+# print(tup1)
+# print(type(tup1))
+
+# dict1 = {}
+# print(type(dict1))
+# print(dict1)
+
+# dict1["virat"] = 100
+# print(dict1)
+
